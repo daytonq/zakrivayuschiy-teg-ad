9f83be9f9cd891f16d74777cd2ab47185063ad10
daytonq <uravolkov28@gmail.com>
2026-05-26 18:28:31 +0300

    fix: prevent dialog form submit

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,6 +1,19 @@
 const saveButton = document.querySelector('.save-button');
 const saveDialog = document.querySelector('.dialog');
+const dialogForm = saveDialog.querySelector('form');
+const dialogCloseButton = saveDialog.querySelector('.dialog__button');
 
-saveButton.addEventListener('click', () => {
+saveButton.addEventListener('click', (event) => {
+  event.preventDefault();
   saveDialog.showModal();
+});
+
+dialogCloseButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  saveDialog.close();
+});
+
+dialogForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  saveDialog.close();
 });
